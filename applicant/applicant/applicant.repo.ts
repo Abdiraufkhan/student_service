7ad89c3ec64faa871a91b47e70e09b1b9fcedce9
lazizbecode <shukhratovich001@gmail.com>
2023-06-08 11:06:31 +0500

    ch

---
+++ applicant/applicant/applicant.repo.ts
@@ -0,0 +1,27 @@
+import User from "domain/user/user.model";
+import { CreateApplicantDto} from "./applicant.dto";
+import Post from "./applicant.model";
+import Applicant from "./applicant.model";
+
+class ApplicantRepo {
+    public getAll(): Promise<Applicant[]> {
+        return Applicant.findAll();
+    }
+
+    public getById(id: string): Promise<Applicant> {
+        return Applicant.findOne({
+            where: {
+                id,
+            },
+            raw: true,
+        });
+    }
+
+    public create(data: CreateApplicantDto): Promise<Applicant> {
+        return Applicant.create({
+            ...data,
+        });
+    }
+}
+
+export default ApplicantRepo;
